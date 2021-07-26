04ee1078f2e4f992bfd92bd2231f2c587e06606d
t106362512 <t106362512@gmail.com>
2021-07-27 06:48:51 +0800

    fix(HW3 File server): 🐛 Fix playbook and ftp-watchd script.

--- nctu-hw/plays/files/ftp/uploadscript.sh
+++ nctu-hw/plays/files/ftp/uploadscript.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-echo `date` ${UPLOAD_USER} has upload file ${1} with size ${UPLOAD_SIZE} >> /var/log/uploadscript.log
+ftp_log="$(date) ${UPLOAD_USER} has upload file ${1} with size ${UPLOAD_SIZE}" 
+echo "${ftp_log}" >> /var/log/uploadscript.log
