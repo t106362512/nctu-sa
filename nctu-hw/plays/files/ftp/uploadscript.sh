75f1d4b7c6a02f1eb672e1bff7e37d022ca1fe31
t106362512 <t106362512@gmail.com>
2021-08-04 03:53:41 +0800

    fix(HW5 NFS server): 🐛 修正部分腳本, 以符合新生訓練之內容.
    關閉 ldap 相關服務, 使用 autofs 為 client 掛載 nfs. 還有為 2020 sa 作業 5-2 修改上傳腳本之內容, 以將在 nfs.1.nctu.cs 上的每位使用者上傳之 ftp 記錄丟到 nfs 上, 最後再讓作為 nfs client 的 www.1.nctu.cs 在網頁上顯示每位使用者上傳紀錄.

--- nctu-hw/plays/files/ftp/uploadscript.sh
+++ nctu-hw/plays/files/ftp/uploadscript.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 ftp_log="$(date) ${UPLOAD_USER} has upload file ${1} with size ${UPLOAD_SIZE}" 
 echo "${ftp_log}" >> /var/log/uploadscript.log
+
+user_dir="/home/${UPLOAD_USER}"
+upload_dir="/vol/web_hosting/${user_dir}/public_html"
+! [ -f "${upload_dir}" ] && mkdir -p "${upload_dir}"
+echo "${ftp_log}" >> "${upload_dir}/uploadscript.log"
